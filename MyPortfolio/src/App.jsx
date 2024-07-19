3a5832748c17e9f71f5f22aa13ff72c05cdfbf20
Adhishthan <adhishthanashok@gmail.com>
2024-07-19 20:26:17 +0530

    Added comment

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -1,25 +1,24 @@
-import NavBar from "./components/NavBar"
-import Home from "./components/Home"
-import SocialLinks from "./components/SocialLinks"
-import About from "./components/About"
-import Portfolio from "./components/Portfolio"
-import Experience from "./components/Experience"
-import Contact from "./components/Contact"
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 
 function App() {
-
   return (
+    // Added Components below
     <>
-     <NavBar />
-     <Home />
-     <About />
-     <Portfolio />
-     <Experience />
-     <Contact />
-
-     <SocialLinks />
+      <NavBar />
+      <Home />
+      <About />
+      <Portfolio />
+      <Experience />
+      <Contact />
+      <SocialLinks />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
