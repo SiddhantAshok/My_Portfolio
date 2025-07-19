67402d11eee79ffb15f746aeb6ffeaa374203aef
Siddhant Ashok <siddhant.ashok091994@gmail.com>
2025-07-19 23:52:18 +0530

    Footer elements on hover

--- MyPortfolio/src/components/Footer.js
+++ MyPortfolio/src/components/Footer.js
@@ -5,9 +5,47 @@ class Footer extends Component {
     if (this.props.sharedBasicInfo) {
       var networks = this.props.sharedBasicInfo.social.map(function (network) {
         return (
-          <span key={network.name} className="m-4">
+          <span 
+            key={network.name} 
+            className="m-4"
+            style={{ position: "relative", display: "inline-block" }}
+            onMouseEnter={(e) => {
+              e.currentTarget.querySelector("i").style.transform = "scale(1.2)";
+              e.currentTarget.querySelector(".tooltip").style.opacity = "1";
+            }}
+            onMouseLeave={(e) => {
+              e.currentTarget.querySelector("i").style.transform = "scale(1)";
+              e.currentTarget.querySelector(".tooltip").style.opacity = "0";
+            }}
+          >
+            <span
+              style={{
+                position: "absolute",
+                top: "-2rem",
+                left: "50%",
+                transform: "translateX(-50%)",
+                backgroundColor: "#1F2937",
+                color: "white",
+                fontSize: "1.1rem",
+                padding: "0.25rem 0.5rem",
+                borderRadius: "0.25rem",
+                opacity: 0,
+                transition: "opacity 0.3s ease",
+                pointerEvents: "none",
+                whiteSpace: "nowrap",
+              }}
+              className="tooltip"
+            >
+              {network.name}
+            </span>
             <a href={network.url} target="_blank" rel="noopener noreferrer">
-              <i className={network.class}></i>
+              <i 
+                className={network.class}
+                style={{
+                  transition: "transform 0.3s ease",
+                  display: "inline-block",
+                }}
+              ></i>
             </a>
           </span>
         );
